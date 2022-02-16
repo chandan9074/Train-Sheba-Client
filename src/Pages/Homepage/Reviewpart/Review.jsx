9c5d9d1a6bac755e4db2193053b13a0e32b5cc2f
chandan9074 <chandan15-11611@diu.edu.bd>
2022-02-16 11:08:05 +0600

    refact: get review data from db and show them in the ui

--- src/Pages/Homepage/Reviewpart/Review.jsx
+++ src/Pages/Homepage/Reviewpart/Review.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, EffectCoverflow } from 'swiper';
@@ -8,6 +8,7 @@ import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
 import './review.css';
 import Rating from 'react-rating';
+import { useState } from 'react';
 // Import Swiper styles
 // import "swiper/css";
 // import "swiper/css/effect-coverflow"
@@ -20,6 +21,15 @@ SwiperCore.use([EffectCoverflow,Pagination,Autoplay]);
 
 const Review = () => {
     const sample = [1, 2, 3, 4, 5, 6]
+    const [review, setReview] = useState([]);
+    useEffect(()=>{
+        fetch("http://localhost:5000/review")
+        .then(res=>res.json())
+        .then(data=> {
+            // const newArray = data.slice(-6);
+            setReview(data)
+        })
+    })
     return ( 
         <div className="">
         <div className="container pt-16 pb-20 ">
@@ -42,20 +52,20 @@ const Review = () => {
             "modifier": 1,
             "slideShadows": true
             }} pagination={true} className="mySwiper">
-                {sample.map(sam=>
+                {review.map(data=>
 
             <SwiperSlide>
                 <div className="bg-gray-500 h-full flex flex-col items-center justify-center p-5">
-                    <img src="https://i.ibb.co/N3TMdh6/Handsome-mid-adult-man-with-beard-standing-with-crossed-hands-and-looking-at-camera-Mature-middle-ea.jpg" alt="" className="w-24 h-24 rounded-full mb-3"  />
+                    {data.user_img && !data.anony ? <img src={data.user_img} alt="" className="w-24 h-24 rounded-full mb-3"  />: <i className="fas fa-user text-5xl mb-3"></i> }
                     <Rating
                         readonly
-                        placeholderRating={3.5}
+                        placeholderRating={data.rating}
                         emptySymbol={<i className="far fa-star text-yellow-400"></i>}
                         placeholderSymbol={<i className="fas fa-star text-yellow-400"></i>}
                         fullSymbol={<i className="fas fa-star text-yellow-400"></i>}
                     />
-                    <p className="text-center my-2 text-sm font-medium elipsis-after-4-line w-11/12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis officia, id architecto explicabo deserunt eos id architecto explicabo deserunt eos. id architecto explicabo deserunt eos id architecto explicabo deserunt eos</p>
-                    <h6 className="mt-3 text-base font-bold">~ Chandan</h6>
+                    <p className="text-center my-2 text-sm font-medium elipsis-after-4-line w-11/12">{data.review}</p>
+                    {data.anony ? <h6 className="mt-3 text-base font-bold">~ Anonymous</h6>: <h6 className="mt-3 text-base font-bold">~ {data.name}</h6>}
                 </div>
             </SwiperSlide>)}
             {/* <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide>
